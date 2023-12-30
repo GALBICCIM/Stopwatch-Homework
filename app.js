8704c885f7cb36bb9e1a9e920c5d862fb41512dc
GALBICCIM <godvictory0909@gmail.com>
2023-12-30 18:16:11 +0900

    js에 주석 추가 및 타이틀 수정

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 let startTime, elapsedTime = 0, timerInterval;
-let lapNumber = 1;
+let lapNumber = 1;  // 랩 나열을 위한 변수
 
 
+// 시작
 function startStopwatch() {
+    // 시작 버튼을 눌렀을 때 시작을 제외한 모든 버튼 표시
     document.getElementById('startBtn').style.display = 'none';
     document.getElementById('lapBtn').style.display = 'inline-block';
     document.getElementById('stopBtn').style.display = 'inline-block';
     document.getElementById('resetBtn').style.display = 'inline-block';
 
     startTime = Date.now() - elapsedTime;
+
     timerInterval = setInterval(function () {
         elapsedTime = Date.now() - startTime;
         displayTime(elapsedTime);
@@ -16,43 +19,49 @@ function startStopwatch() {
 }
 
 
+// 일시정지, 재시작 표시
 function togglePause() {
     const stopBtn = document.getElementById('stopBtn');
+
     if (stopBtn.innerText === '일시정지') {
         clearInterval(timerInterval);
         stopBtn.innerText = '재시작';
         document.getElementById('lapBtn').style.display = 'none';
     } else {
         startTime = Date.now() - elapsedTime;
+
         timerInterval = setInterval(function () {
             elapsedTime = Date.now() - startTime;
             displayTime(elapsedTime);
         }, 10);
+
         stopBtn.innerText = '일시정지';
         document.getElementById('lapBtn').style.display = 'inline-block';
     }
 }
 
 
+// 초기화
 function resetStopwatch() {
     clearInterval(timerInterval);
+
     elapsedTime = 0;
     lapNumber = 1;
+
     displayTime(elapsedTime);
     displayLaps([]);
-    document.getElementById('stopBtn').style.display = 'none';
-    document.getElementById('resetBtn').style.display = 'none';
-    document.getElementById('lapBtn').style.display = 'none';
-    document.getElementById('startBtn').style.display = 'inline-block';
-    location.reload();
+    
+    location.reload();  // 초기화 버튼을 눌렀을 때 처음 페이지로 이동
 }
 
 
+// 랩 기록
 function recordLap() {
     const lapTime = elapsedTime;
     const formattedLapTime = formatTime(lapTime);
     const lapText = `Lap ${lapNumber}: ${formattedLapTime}`;
 
+    // lapOutput div에 출력
     const lapOutput = document.getElementById('lapOutput');
     const lapItem = document.createElement('div');
     lapItem.innerText = lapText;
@@ -62,6 +71,7 @@ function recordLap() {
 }
 
 
+// 시간을 화면에 보여주는 함수
 function displayTime(time) {
     const seconds = Math.floor(time / 1000);
     const remainingMilliseconds = time % 1000;
@@ -73,6 +83,7 @@ function displayTime(time) {
 }
 
 
+// 시간 기록 과정
 function formatTime(time) {
     const seconds = Math.floor(time / 1000);
     const remainingMilliseconds = time % 1000;
@@ -82,9 +93,11 @@ function formatTime(time) {
 }
 
 
+// 랩 기록을 화면에 출력하기 위한 함수
 function displayLaps(lapArray) {
     const lapOutput = document.getElementById('lapOutput');
     lapOutput.innerHTML = '';
+    
     lapArray.forEach((lap, index) => {
         const lapItem = document.createElement('div');
         lapItem.innerText = `Lap ${index + 1}: ${lap}`;
